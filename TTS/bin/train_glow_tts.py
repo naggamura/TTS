6a7ded1739f4f9ad9a4032630c8f4fe4cdf473b4
naggamura <124233502+naggamura@users.noreply.github.com>
2023-03-02 16:47:47 +0900

    Update train_glow_tts.py

--- TTS/bin/train_glow_tts.py
+++ TTS/bin/train_glow_tts.py
@@ -414,6 +414,7 @@ def evaluate(data_loader, model, criterion, ap, global_step, epoch):
 
     if args.rank == 0 and epoch >= config.test_delay_epochs:
         if config.test_sentences_file:
+            print(config.test_sentences_file)
             with open(config.test_sentences_file, "r") as f:
                 test_sentences = [s.strip() for s in f.readlines()]
         else:
